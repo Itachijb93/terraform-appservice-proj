5313c925136ab6458f78875d1828b3e5dddc6de5
laksh <laxmanmuddam1304@gmail.com>
2025-12-14 07:42:56 +0530

    Updated canary deployment

--- reactapp/outputs.tf
+++ reactapp/outputs.tf
@@ -8,30 +8,3 @@ output "canary_url" {
 
 
 
-
-
-# # output "production_url" {
-# #   value       = azurerm_app_service.app.default_site_hostname
-# #   description = "URL of production app"
-# # }
-
-# # output "staging_url" {
-# #   value       = azurerm_app_service_slot.staging.default_site_hostname
-# #   description = "URL of staging app"
-# # }
-
-# output "appservice_url" {
-#   value = azurerm_app_service.app.default_site_hostname
-# }
-
-# output "staging_slot_url" {
-#   value = azurerm_app_service_slot.staging.default_site_hostname
-# }
-
-# output "state_storage_account" {
-#   value = azurerm_storage_account.tfstate.name
-# }
-
-# output "state_container" {
-#   value = azurerm_storage_container.tfstate.name
-# }
--- reactapp/variables.tf
+++ reactapp/variables.tf
@@ -25,31 +25,3 @@ variable "canary_zip_path" {
 }
 
 
-
-
-
-
-# variable "subscription_id" {
-#   description = "The Azure Subscription ID where resources will be created."
-#   type        = string
-# }
-
-# variable "prefix" {
-#   description = "Prefix for all resources"
-#   type        = string
-# }
-
-# variable "location" {
-#   description = "Azure location"
-#   type        = string
-# }
-
-# variable "prod_zip_path" {
-#   description = "Path to production zip"
-#   type        = string
-# }
-
-# variable "staging_zip_path" {
-#   description = "Path to staging zip"
-#   type        = string
-# }
